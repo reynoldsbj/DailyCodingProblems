d818b41e201cf6e619c364d72c2fc7620f291dd8
reynoldsbj <reynoldsbj.developer@gmail.com>
2020-04-03 00:07:19 -0400

    Initial setup Problem001

---
+++ Problem001/Problem001.UnitTests/Problem001_UnitTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Problem001.UnitTests
+{
+    [TestClass]
+    public class Problem001_UnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
